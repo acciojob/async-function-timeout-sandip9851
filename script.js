5658cb87b4d43388331f958c527fa088c0a5ef5d
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-06-26 17:05:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+document.getElementById("btn").addEventListener("click",display)
+async function display() {
+let value = document.getElementById("text").value;
+let delay = document.getElementById("delay").value;
+
+await createPromise(value,delay)
+
+}
+
+function createPromise(value,delay) {
+return new Promise((resolve,reject)=>{
+setTimeout(()=>{
+document.getElementById("output").innerText = value;
+resolve()
+},delay)
+
+})
+}
